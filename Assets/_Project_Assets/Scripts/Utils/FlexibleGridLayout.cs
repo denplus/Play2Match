408f21a21e3a19b1437ac979d127a44a526dd60c
Denys Tiuzniev <denplusmail@gmail.com>
2024-08-22 13:00:42 +0200

    set up flexible grid logic for card layout

--- Assets/_Project_Assets/Scripts/Utils/FlexibleGridLayout.cs
+++ Assets/_Project_Assets/Scripts/Utils/FlexibleGridLayout.cs
@@ -5,8 +5,10 @@ namespace Scripts.Utils
 {
     public class FlexibleGridLayout : GridLayoutGroup
     {
-        [SerializeField] private int constrains; // to change click 3 dots and select debug mode render from inspector
-        
+        // to change click 3 dots and select debug mode render from inspector
+        [field: SerializeField] public int AmountX { get; set; }
+        [field: SerializeField] public int AmountY { get; set; }
+
         public override void SetLayoutHorizontal()
         {
             UpdateCellSize();
@@ -22,9 +24,10 @@ namespace Scripts.Utils
         private void UpdateCellSize()
         {
             constraint = Constraint.FixedColumnCount;
-            constraintCount = constrains;
+            constraintCount = AmountX;
             float x = (rectTransform.rect.size.x - padding.horizontal - spacing.x * (constraintCount - 1)) / constraintCount;
-            cellSize = new Vector2(x, cellSize.y);
+            float sizeY = (Screen.height / (float)AmountY) - padding.bottom - padding.top;
+            cellSize = new Vector2(x, sizeY);
         }
     }
 }
