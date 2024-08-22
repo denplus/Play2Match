048e8a440551048c4aee9c93788d53d8938c44f7
Denys Tiuzniev <denplusmail@gmail.com>
2024-08-22 12:38:35 +0200

    set up script folder

---
+++ Assets/_Project_Assets/Scripts/Utils/FlexibleGridLayout.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.Utils
+{
+    public class FlexibleGridLayout : GridLayoutGroup
+    {
+        [SerializeField] private int constrains; // to change click 3 dots and select debug mode render from inspector
+        
+        public override void SetLayoutHorizontal()
+        {
+            UpdateCellSize();
+            base.SetLayoutHorizontal();
+        }
+
+        public override void SetLayoutVertical()
+        {
+            UpdateCellSize();
+            base.SetLayoutVertical();
+        }
+
+        private void UpdateCellSize()
+        {
+            constraint = Constraint.FixedColumnCount;
+            constraintCount = constrains;
+            float x = (rectTransform.rect.size.x - padding.horizontal - spacing.x * (constraintCount - 1)) / constraintCount;
+            cellSize = new Vector2(x, cellSize.y);
+        }
+    }
+}
